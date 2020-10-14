9204303ac09d2ae83cfedfd29388b9b261159e26
pjoon86 <pjoon86@naver.com>
2020-10-15 01:46:39 +0900

    채팅 보내고 받기

--- node/app.js
+++ node/app.js
@@ -8,6 +8,19 @@ app.get('/', ( _ ,res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen( port, () => {
+const server = app.listen( port, () => {
     console.log('Express listening on port', port);
+});
+
+const listen = require('socket.io');
+const io = listen(server);
+
+io.on('connection', (socket) => { 
+
+    socket.on('client message', (data) => {
+        io.emit('server message', {
+            message : data.message
+        });
+    });
+
 });
